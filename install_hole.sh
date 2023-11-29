980cee9e4bb2f03e27fc3ea9d2155d8a7a6ccccc
guoyaobit <guoyaobit@gmail.com>
2023-11-30 03:47:24 +0800

    fix install shell path

--- install_hole.sh
+++ install_hole.sh
@@ -1,4 +1,3 @@
 wget -O  hole2.tar.gz http://www.holeprogram.org/downloads/2.2.005/hole2-ApacheLicense-2.2.005-Linux-x86_64.tar.gz
-tar xf hole2.tar.gz
+tar xf hole2.tar.gz -C ~/
 echo "PATH=\$PATH:~/hole2/exe" >> ~/.bashrc
-source ~/.bashrc
