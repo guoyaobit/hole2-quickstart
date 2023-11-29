d4e5f153ccb10f1b1078d23648d58e47bba3656c
guoyaobit <guoyaobit@gmail.com>
2023-11-30 03:24:18 +0800

    linux shell for install hole2

---
+++ install_hole.sh
@@ -0,0 +1,4 @@
+wget -O  hole2.tar.gz http://www.holeprogram.org/downloads/2.2.005/hole2-ApacheLicense-2.2.005-Linux-x86_64.tar.gz
+tar xf hole2.tar.gz
+echo "PATH=\$PATH:~/hole2/exe" >> ~/.bashrc
+source ~/.bashrc
