52527830f562c8dfe521d9a7061bf525ba6b68a1
guoyaobit <guoyaobit@gmail.com>
2023-11-30 23:34:44 +0800

    Fix: Use pexpect to run qpt_conv

--- run_holeprogram.py
+++ run_holeprogram.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
 import shutil
-from multiprocessing import Pool
+import pexpect
+# from multiprocessing import Pool
 def run_hole(m_pdb):
     os.chdir(current_path)
     foldername = os.path.splitext(m_pdb)[0]
@@ -22,23 +23,14 @@ def run_hole(m_pdb):
     command = "sph_process -dotden 15 -color hole_out.sph dotsurface.qpt"
     subprocess.run(command, shell=True, check=True, text=True)
     command = "qpt_conv"
-    process=subprocess.Popen(command, shell=True,stdin=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    commands = [
-    'D',
-    '\n',
-    '\n',
-    '1',
-    ]
-    for command in commands:
-        process.stdin.write(f'{command}\n')
-    process.stdin.close()
-    # output, error = process.communicate()
-    # # Print the output and error
-    # print("Output:")
-    # print(output)
-
-    # print("\nError:")
-    # print(error)
+    process=pexpect.spawn(command)
+    
+    process.write("D\n")
+    process.write("\n")
+    process.write("\n")
+    process.write("1\n")
+    process.close()
+    
     command ="sph_process -sos -dotden 15 -color hole_out.sph solid_surface.sos"
     subprocess.run(command,shell=True, check=True, text=True)
     command ="sos_triangle -s < solid_surface.sos > solid_surface.vmd_plot"
@@ -46,7 +38,7 @@ def run_hole(m_pdb):
 
 current_path = os.getcwd()
 pdb_files = [file for file in os.listdir(current_path) if file.endswith(".pdb")]
-# print(pdb_files)
+print(pdb_files)
 # folders_to_zip = [os.path.splitext(file)[0] for file in pdb_files]
 # print(folders_to_zip)
 for i in pdb_files:
